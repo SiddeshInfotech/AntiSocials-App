272fd9d202b51548e04c399716937d323be63abc
Prabhat Mishra <mprabhat1607@gmail.com>
2026-03-17 16:43:25 +0530

    Created Activitie Page also + form of activitie

--- Frontend/app/otp.tsx
+++ Frontend/app/otp.tsx
@@ -109,6 +109,7 @@ export default function OTPScreen() {
                 isComplete ? styles.buttonActive : styles.buttonDisabled
               ]}
               disabled={!isComplete}
+              onPress={() => router.replace('/(tabs)')}
             >
               <Text style={[
                 styles.buttonText,
